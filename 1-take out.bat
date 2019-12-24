b1dbbb79ad9fcb4caa9daa5453982e052d15c1ce
EarlyAnt <54763755@qq.com>
2019-12-24 18:01:58 +0800

    1.上传2个测试用的批处理文件。

---
+++ 1-take out.bat
@@ -0,0 +1,4 @@
+@echo off
+move ./Assets/art_assets/Ani/Role ./Resources/Role
+::move ./Assets/ShaderForge ./Resources/ShaderForge
+::move ./Assets/Shaders ./Resources/Shaders
---
+++ 2-put in.bat
@@ -0,0 +1,4 @@
+@echo off
+move ./Resources/Role ./Assets/art_assets/Ani/Role
+::move ./Resources/ShaderForge ./Assets/ShaderForge
+::move ./Resources/Shaders ./Assets/Shaders
