0bc505b27f66e944852449c33765911a5fd3d6ba
EarlyAnt <54763755@qq.com>
2020-04-17 18:22:15 +0800

    1.上传从主项目中拷过来的6个表情的图片，动画状态控制机和预制体等文件。 2.新增ExpressionLoader脚本，用于加载表情AB包并显示表情。 3.新建Expressions场景，用于调试表情AB包的加载和显示。

---
+++ Assets/Scripts/TestUtils/ABLoader/ExpressionLoader.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ABLoader
+{
+    public class ExpressionLoader : BaseLoader
+    {
+        [SerializeField]
+        private string objectName;
+        [SerializeField]
+        private bool SetParent = false;
+
+        [ContextMenu("0-创建物体")]
+        public override void LoadNextAB()
+        {
+            this.assetBundle = this.GetAssetBundle(this.assetBundleName);
+            Object obj = this.assetBundle.LoadAsset(this.objectName);
+            this.gameObject = GameObject.Instantiate(obj) as GameObject;
+            this.gameObject.name = this.objectName;
+            if (this.SetParent)
+            {
+                this.gameObject.transform.SetParent(this.transform, false);
+                this.gameObject.transform.localEulerAngles = Vector3.zero;
+            }
+            else
+            {
+                this.gameObject.transform.position = this.transform.position;
+                this.gameObject.transform.rotation = Quaternion.identity;
+            }
+            this.gameObject.AddComponent<FixShader>();
+            Debug.LogFormat("<><ABLoader.LoadAB>asset bundle name: {0}, pet name: {1}", this.assetBundleName, this.objectName);
+            base.LoadNextAB();
+        }
+    }
+}
