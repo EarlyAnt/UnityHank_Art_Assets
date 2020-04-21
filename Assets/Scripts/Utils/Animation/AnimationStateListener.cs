c404997ff6ce034f9d0e51aa3c9af5f8376368dc
EarlyAnt <54763755@qq.com>
2020-04-22 05:40:25 +0800

    1.添加AnimationStateListener脚本，用于监控表情动画播放的状态，并在结束时发出信号。 2.添加StateBehaviourTest脚本，用于测试从项目中加载spine动画，及从外部assetbundle包中加载spine动画，能否正常获取到AnimationStateListener脚本。

---
+++ Assets/Scripts/Utils/Animation/AnimationStateListener.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class AnimationStateListener : StateMachineBehaviour
+{
+    [SerializeField]
+    private string animationName;
+    public string AnimationName { get { return this.animationName; } }
+    [SerializeField]
+    private bool exitAnimation;
+    public bool ExitAnimation { get { return this.exitAnimation; } }
+    public System.Action<string, bool> AnimationExit;
+    public System.Action<Animator, AnimatorStateInfo, int> AnimationStateExit;
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    if (stateInfo.length == stateInfo.normalizedTime)
+    //        Debug.LogFormat("<><AnimationStateListener.OnStateUpdate>stateInfo.normalizedTime: {0}, stateInfo.length: {1}", stateInfo.normalizedTime, stateInfo.length);
+    //}
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.LogFormat("<><AnimationStateListener.OnStateExit>stateInfo.normalizedTime: {0}, stateInfo.length: {1}", stateInfo.normalizedTime, stateInfo.length);
+
+        if (this.AnimationExit != null)
+        {
+            Debug.LogWarningFormat("<><AnimationStateListener.OnStateExit>AnimationExit, animationName: {0}, exitAnimation: {1}", this.animationName, this.exitAnimation);
+            this.AnimationExit(this.animationName, this.exitAnimation);
+        }
+
+        if (this.AnimationStateExit != null)
+        {
+            Debug.LogWarningFormat("<><AnimationStateListener.OnStateExit>AnimationStateExit, animator: {0}, stateInfo: {1}, layerIndex: {2}", animator, stateInfo, layerIndex);
+            this.AnimationStateExit(animator, stateInfo, layerIndex);
+        }
+    }
+
+    //OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    Debug.LogFormat("<><AnimationStateListener.OnStateMove>stateInfo.normalizedTime: {0}, stateInfo.length: {1}", stateInfo.normalizedTime, stateInfo.length);
+    //}
+
+    //OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
---
+++ Assets/Scripts/Utils/Animation/StateBehaviourTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateBehaviourTest : MonoBehaviour
+{
+    void Start()
+    {
+        Animator animator = this.GetComponent<Animator>();
+        StateMachineBehaviour[] stateMachineBehaviours = animator.GetBehaviours<StateMachineBehaviour>();
+        Debug.LogFormat("<><StateBehaviourTest.Start>stateMachineBehaviours: {0}", stateMachineBehaviours.Length);
+        AnimationStateListener[] animationStateListeners = animator.GetBehaviours<AnimationStateListener>();
+        Debug.LogFormat("<><StateBehaviourTest.Start>animationStateListeners: {0}", animationStateListeners.Length);
+    }
+}
