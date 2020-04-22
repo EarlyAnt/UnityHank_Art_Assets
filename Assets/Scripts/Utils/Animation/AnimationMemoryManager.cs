a62a74ec58e49c2574695c730e1280277a6575db
EarlyAnt <54763755@qq.com>
2020-04-22 16:36:38 +0800

    1.增加AnimationMemoryManager脚本和AnimationMemory.unity场景，调试回收表情动画的sprite。 2.事实证明，无需自己回收帧动画的图集，只要保证先删除和卸载之前的表情，再创建新的表情，就不会错乱，assetBundle.Unload(true)方法是可以完全卸载资源的。

---
+++ Assets/Scripts/Utils/Animation/AnimationMemoryManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMemoryManager : MonoBehaviour
+{
+    /************************************************属性与变量命名************************************************/
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private List<Sprite> sprites = new List<Sprite>();
+    /************************************************Unity方法与事件***********************************************/
+    private void Start()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (this.spriteRenderer != null && !this.sprites.Contains(this.spriteRenderer.sprite))
+        {
+            this.sprites.Add(this.spriteRenderer.sprite);
+            Debug.LogFormat("<><AnimationMemoryManager.Update>add sprite: {0}", this.spriteRenderer.sprite);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Animator animator = this.GetComponent<Animator>();
+            if (animator != null) animator.SetTrigger("Exit");
+            Debug.Log("<><AnimationMemoryManager.Update>play exit animation");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            for (int i = 0; i < this.sprites.Count; i++)
+            {
+                Resources.UnloadAsset(this.sprites[i]);
+            }
+            Resources.UnloadUnusedAssets();
+            GameObject.Destroy(this.gameObject);
+            Debug.Log("<><AnimationMemoryManager.Update>collect memory");
+        }
+    }
+    /************************************************自 定 义 方 法************************************************/
+}
