7f87409e2f08fda003314eba1bfdb7496f05e72e
EarlyAnt <54763755@qq.com>
2020-10-14 16:48:18 +0800

    1.增加SpineAnimation场景。 2.增加SpineTest脚本，实现spine动画的播放。 3.制作spine动画的预制体，并输出成assetbundle。 4.已解决加载spine动画的ab包后，材质球丢失的问题。

---
+++ Assets/Scripts/Test/SpineTest.cs
@@ -0,0 +1,51 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpineTest : MonoBehaviour
+{
+    [SerializeField]
+    private SkeletonGraphic spineGraphic;
+    [SerializeField]
+    private string startAnimationName;
+    private List<string> animations = new List<string>();
+    private int animationIndex = 0;
+
+    private void Start()
+    {
+        if (this.spineGraphic == null)
+            this.spineGraphic = this.GetComponent<SkeletonGraphic>();
+
+        foreach (var animation in this.spineGraphic.SkeletonData.Animations)
+        {
+            this.animations.Add(animation.name);
+        }
+        this.PlayAnimation(!string.IsNullOrEmpty(this.startAnimationName) ? this.startAnimationName : this.animations[0]);
+    }
+
+    private void Update()
+    {
+        if (this.spineGraphic == null || this.animations.Count == 0)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.animationIndex = (this.animationIndex + this.animations.Count - 1) % this.animations.Count;
+            this.PlayAnimation(this.animations[this.animationIndex]);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.animationIndex = (this.animationIndex + 1) % this.animations.Count;
+            this.PlayAnimation(this.animations[this.animationIndex]);
+        }
+    }
+
+    private void PlayAnimation(string animationName)
+    {
+        //this.spineGraphic.AnimationState.ClearTracks();//这里不能ClearTracks，否则电池的所有格子不能全部正常显示
+        this.spineGraphic.AnimationState.SetAnimation(0, animationName, true);
+        Debug.LogFormat("<><SpineTest.PlayAnimation>currrent animation: {0}", animationName);
+    }
+}
+
---
+++ Assets/Scripts/TestUtils/ABLoader/SpineAnimatonLoader.cs
@@ -0,0 +1,45 @@
+﻿using Common;
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ABLoader
+{
+    public class SpineAnimatonLoader : BaseLoader
+    {
+        [SerializeField]
+        private string objectName;        
+        [SerializeField]
+        private bool setParent = false;
+        private const string MATERIAL_NAME = "SkeletonGraphicDefault";
+
+        [ContextMenu("0-创建物体")]
+        public override void LoadAB()
+        {
+            this.assetBundle = this.GetAssetBundle(this.assetBundleName);
+            Object spineObject = this.assetBundle.LoadAsset(this.objectName);
+            Object materialObject = this.assetBundle.LoadAsset(MATERIAL_NAME);
+            Material material = materialObject as Material;
+            Shader shader = Shader.Find(material.shader.name);
+            material.shader = shader;
+            SkeletonGraphic spine = (spineObject as GameObject).GetComponent<SkeletonGraphic>();
+            spine.material = material;
+            this.gameObject = GameObject.Instantiate(spineObject) as GameObject;
+            this.gameObject.name = this.objectName;
+            if (this.setParent)
+            {
+                this.gameObject.transform.SetParent(this.transform, false);
+                this.gameObject.transform.localEulerAngles = Vector3.zero;
+            }
+            else
+            {
+                this.gameObject.transform.position = this.transform.position;
+                this.gameObject.transform.rotation = Quaternion.identity;
+            }
+            this.gameObject.AddComponent<SpineTest>();
+            Debug.LogFormat("<><SpineAnimatonLoader.LoadAB>assetbundle: {0}, object: {1}", this.assetBundleName, this.objectName);
+            base.LoadAB();
+        }
+    }
+}
