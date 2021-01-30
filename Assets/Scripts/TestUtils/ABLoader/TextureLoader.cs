8f4a27d79c042177f63588e22f6d8fb5e97f5790
EarlyAnt <54763755@qq.com>
2021-01-30 18:04:16 +0800

    1.导入骑行游戏的地图和路线图片。 2.新增TextureLoader.cs脚本，实现从assetbundle包中读取图片并显示。 3.经测试发现，将图片打成assetbundle包，是可以保留图片的压缩设置的。从外部动态加载assetbundle包并读取图片，也能很快的显示。

---
+++ Assets/Scripts/TestUtils/ABLoader/TextureLoader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ABLoader
+{
+    public class TextureLoader : BaseLoader
+    {
+        [SerializeField]
+        private string spriteName;
+        [SerializeField]
+        private RawImage imageBox;
+
+        public override void LoadAB()
+        {
+            this.assetBundle = this.GetAssetBundle(this.assetBundleName);
+            Object textureObject = this.assetBundle.LoadAsset(this.spriteName);
+            Texture2D texture = textureObject as Texture2D;
+            this.imageBox.texture = texture;
+            this.imageBox.enabled = true;
+            Debug.LogFormat("<><TextureLoader.LoadAB>assetbundle: {0}, sprite: {1}", this.assetBundleName, this.spriteName);
+            base.LoadAB();
+        }
+
+        [ContextMenu("0-设置Image组件")]
+        private void CollectImageBox()
+        {
+            this.imageBox = this.GetComponent<RawImage>();
+        }
+    }
+}
