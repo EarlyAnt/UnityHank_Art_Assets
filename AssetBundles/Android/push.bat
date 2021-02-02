5f65c0bec3a0461a85c465f711aa9a8b8f3be18b
EarlyAnt <54763755@qq.com>
2021-02-02 10:38:26 +0800

    1.编译appgame的南京地图的ab包文件。 2.整理推送地图ab文件及页面png图片的push命令脚本。

---
+++ AssetBundles/Android/push.bat
@@ -0,0 +1,7 @@
+adb shell rm -r /storage/self/primary/Android/data/com.bowhead.appgame/files/Model/cycling
+adb shell mkdir /storage/self/primary/Android/data/com.bowhead.appgame/files/Model/cycling
+
+
+adb push "cycling/nanjing.ab" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Model/cycling/nanjing.ab"
+
+pause
