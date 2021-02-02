45a5e4ee6966203b269ae15b3f5f9d1ada007053
EarlyAnt <54763755@qq.com>
2021-02-02 13:22:31 +0800

    1.导入卡片临时图片map4.png。 2.nanjing_bg，path01，path02，map4都选用"RGBA ASTC 12*12"这个压缩参数，内存占用从11.3M骤减至300K左右。淦！

--- AssetBundles/Android/push.bat
+++
@@ -1,7 +0,0 @@
-adb shell rm -r /storage/self/primary/Android/data/com.bowhead.appgame/files/Model/cycling
-adb shell mkdir /storage/self/primary/Android/data/com.bowhead.appgame/files/Model/cycling
-
-
-adb push "cycling/nanjing.ab" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Model/cycling/nanjing.ab"
-
-pause
