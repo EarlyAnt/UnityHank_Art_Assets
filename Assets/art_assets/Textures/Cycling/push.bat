ad1938d8fa1859e67a04531367cca5c999492520
EarlyAnt <54763755@qq.com>
2021-12-13 13:50:52 +0800

    1.导入AppGame项目的所有图片。 2.将AppGame的图片打包成assetbundle。

---
+++ Assets/art_assets/Textures/Cycling/push.bat
@@ -0,0 +1,16 @@
+adb shell rm -r /storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling
+adb shell mkdir /storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling
+adb shell mkdir /storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View
+
+adb push "View/avatar06.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/avatar06.png"
+adb push "View/avatar09.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/avatar09.png"
+adb push "View/coord.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/coord.png"
+adb push "View/food_chicken_small.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/food_chicken_small.png"
+adb push "View/food_flagjuice_small.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/food_flagjuice_small.png"
+adb push "View/food_opensandwich_small.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/food_opensandwich_small.png"
+adb push "View/food_ricecakefish_small.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/food_ricecakefish_small.png"
+adb push "View/food_salad_small.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/food_salad_small.png"
+adb push "View/food_spider_cake_small.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/food_spider_cake_small.png"
+adb push "View/nanjing_bg.png" "/storage/self/primary/Android/data/com.bowhead.appgame/files/Texture/Cycling/View/nanjing_bg.png"
+
+pause
